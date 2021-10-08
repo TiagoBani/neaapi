02e03c30b459e4cbb9b9dfda2a2f66cd2a05c8ee
tiago.bani <tiago.bani@yahoo.com.br>
2021-10-08 04:57:11 -0300

    feat: add global configs to object mapper

---
+++ src/main/java/com/tiagobani/neaapi/config/ObjectMapperConfig.java
@@ -0,0 +1,19 @@
+package com.tiagobani.neaapi.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    ObjectMapper objectMapper(){
+        var objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+}
