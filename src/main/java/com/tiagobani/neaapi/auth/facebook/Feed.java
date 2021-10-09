57cf524df8d4d1da24675d20bf4927202a5b85e9
tiago.bani <tiago.bani@yahoo.com.br>
2021-10-09 04:14:51 -0300

    feat: add facebook oauth

---
+++ src/main/java/com/tiagobani/neaapi/auth/facebook/Feed.java
@@ -0,0 +1,12 @@
+package com.tiagobani.neaapi.auth.facebook;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Feed {
+
+    private List<Post> data;
+
+}
---
+++ src/main/java/com/tiagobani/neaapi/auth/facebook/Post.java
@@ -0,0 +1,12 @@
+package com.tiagobani.neaapi.auth.facebook;
+
+import lombok.Data;
+
+@Data
+public class Post {
+
+    private String id;
+    private String story;
+    private String message;
+
+}
